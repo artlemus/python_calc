94a7825532c03a370eccc15882c5447997c83562
artlemus <artlemus91@gmail.com>
2020-04-27 20:21:00 -0700

    first commit

--- calc.py
+++ calc.py
@@ -3,14 +3,13 @@ import time
 
 
 
-
 def seperator():
     print("-" * 30)
 
 
 def print_menu():
-    
-    #is there a way to clear the console
+
+    # is there a way to clear the console
     seperator()
     print('Welcome to PyCalc')
     seperator()
@@ -23,8 +22,9 @@ def print_menu():
 
     seperator()
 
+
 def clear_screen():
-    os.system("cls")    
+    os.system("cls")
 
 
 def sum(num1, num2):
@@ -34,21 +34,19 @@ def sum(num1, num2):
 def Subtract(num1, num2):
     return num1 - num2
 
+
 def Multiply(num1, num2):
     return num1 * num2
 
+
 def Divide(num1, num2):
-    
-    if (num1,num2 == 0):
+
+    if (num1, num2 == 0):
         return print('ZeroDivisionError: float division by zero')
-        
-        
-        
-    return num1 / num2 
+
+    return num1 / num2
     time.sleep(3)
     clear_screen()
-
-    
 
 
 opc = ''
@@ -62,12 +60,11 @@ while (opc != 'x'):
 
     num1 = float(input('First number: '))
     num2 = float(input('Second number: '))
-    
+
     if (opc == '1'):
         clear_screen()
         print("Result: " + str(sum(num1, num2)))
         time.sleep(3)
-
 
     elif (opc == '2'):
         clear_screen()
@@ -77,20 +74,19 @@ while (opc != 'x'):
     elif (opc == '3'):
         clear_screen()
         print("Result: " + str(Multiply(num1, num2)))
-        time.sleep(3)  
-    
+        time.sleep(3)
+
     elif (opc == '4'):
         clear_screen()
         print("Result: " + str(Divide(num1, num2)))
         time.sleep(3)
-        
-            
-               
-      
+
 
 input('Press Enter to continue...')
 
 print('Byte Byte!')
+
+
 
 
 # on div show error if the user tries to divide by 0]
